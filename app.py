15a543a08ce87ee262e0ef21fac0834c74996690
Micheal Mbogo <chegemichael003@gmail.com>
2024-08-01 15:47:06 +0300

    second commit

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template
+from website import create_app
 
-#create a flask instance
-app = Flask(__name__)
+app = create_app()
 
-#create route decorator
-@app.route('/')
-def index():
-    return "hello"
+if __name__ == '__main__':
+    app.run(debug=True)
